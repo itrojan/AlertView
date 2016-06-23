a1419dc43d586d7678aa8134b513fd4e4a7e9211
itrojan <itrojan@126.com>
2016-06-23 09:44:16 +0800

    1.0版本

---
+++ alertview/src/main/java/com/aihook/alertview/library/OnDismissListener.java
@@ -0,0 +1,5 @@
+package com.aihook.alertview.library;
+
+public interface OnDismissListener {
+    public void onDismiss(Object o);
+}
---
+++ alertview/src/main/java/com/aihook/alertview/library/OnItemClickListener.java
@@ -0,0 +1,5 @@
+package com.aihook.alertview.library;
+
+public interface OnItemClickListener {
+    public void onItemClick(Object o, int position);
+}
